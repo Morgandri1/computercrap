3dd95d8a7c87fb8cca13d284c89e6f29a369fcf9
Morgan Metz <mlmi4@k12albemarle.org>
2022-07-21 04:52:08 -0400

    Update 3x3.lua

--- 3x3.lua
+++ 3x3.lua
@@ -3,6 +3,7 @@ io.write("what y level would you like the turtle to return to when finished? ")
 top_level = io.read()+60
 current = top_level
 while current ~= 0 do
+    -- cycle
     turtle.digDown()
     turtle.down()
     current = current - 1
@@ -10,14 +11,24 @@ while current ~= 0 do
     turtle.forward()
     turtle.dig()
     turtle.forward()
-    turtle.dig()
-    turtle.forward()
+    -- cycle 2
     turtle.turnLeft()
     turtle.dig()
     turtle.forward()
     turtle.dig()
     turtle.forward()
+    -- cycle 3
+    turtle.turnLeft()
     turtle.dig()
+    turtle.forward()
+    turtle.dig()
+    turtle.forward()
+    -- cycle 4
+    turtle.turnLeft()
+    turtle.dig()
+    turtle.forward()
+    turtle.forward()
+    turtle.turnLeft()
 end
 
 while current ~= top_level do
