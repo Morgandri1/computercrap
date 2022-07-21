dced45e991243d43d53fb50ccbc7cb7ee513b9f3
Morgan Metz <mlmi4@k12albemarle.org>
2022-07-21 04:17:48 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,17 @@
 turtle.refuel()
-while turtle.detectDown() do
+io.write("what y level would you like the turtle to return to when finished? ")
+top_level = io.read()+60
+current = top_level
+while turtle.detectDown() and current ~= 0 do
     turtle.dig()
     turtle.digDown()
     turtle.down()
+    current = current - 1
     turtle.dig()
     turtle.forward()
     turtle.turnLeft()
 end
+
+while current ~= top_level do
+    turtle.digUp()
+    turtle.up()
